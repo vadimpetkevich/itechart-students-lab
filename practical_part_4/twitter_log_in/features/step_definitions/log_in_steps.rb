ef865bcc25355f98fe11865bb4b8b600dd6d908e
Vadim Petkevich <petkevich.minsk@gmail.com>
2015-02-23 20:56:54 +0300

    practical-part-4

--- practical_part_4/twitter_log_in/features/step_definitions/log_in_steps.rb
+++ practical_part_4/twitter_log_in/features/step_definitions/log_in_steps.rb
@@ -1,38 +1,39 @@
-require 'watir-webdriver'
-
-Given('User on the Log in page') do
-  @b = Watir::Browser.start 'https://twitter.com/login'
+Given("I'm on the Log in page") do
+  @b = Watir::Browser.start 'https://vk.com'
 end
 
-When('User enters the invalid data') do
-  login_field = @b.text_field name: 'session[username_or_email]'
-  login_field.when_present.set 'invalid_login'
+When('I enter the invalid data') do
+  login_field = @b.text_field name: 'email'
+  login_field.wait_until_present
+  login_field.set 'invalid_login'
 
-  password_field = @b.text_field name: 'session[password]'
-  password_field.when_present.set 'invalid_password'
+  password_field = @b.text_field name: 'pass'
+  password_field.wait_until_present
+  password_field.set 'invalid_password'
 end
 
-And('User turns log in') do
-  @b.button(:class => "submit btn primary-btn").click
+And('I turn log in') do
+  login_button = @b.button(:id => "quick_login_button")
+  login_button.wait_until_present
+  login_button.click
 end
 
-Then('User should see error message') do
-  puts 'User is on the ' + @b.url
+Then('I should see error message') do
+  @b.div(class: 'login_msg').wait_until_present
   @b.close
 end
 
-When('User enters the existing data') do
-  login_field = @b.text_field name: 'session[username_or_email]'
-  login_field.when_present.set 'vadimpetkevich'
+When('I enter the existing data') do
+  login_field = @b.text_field name: 'email'
+  login_field.wait_until_present
+  login_field.set LOGIN
 
-  password_field = @b.text_field name: 'session[password]'
-  password_field.when_present.set '290514VUS903657'
+  password_field = @b.text_field name: 'pass'
+  password_field.wait_until_present
+  password_field.set PASSWORD
 end
 
-Then('User should see him page') do
-  puts 'User is on the ' + @b.url
+Then('I should see my page') do
+  @b.td(id: 'logout_link_td').wait_until_present
   @b.close
 end
-
-
-
---
+++ practical_part_4/twitter_log_in/features/support/env.rb
@@ -0,0 +1,4 @@
+require 'watir-webdriver'
+
+LOGIN = '375333369338'
+PASSWORD = 'under57'
